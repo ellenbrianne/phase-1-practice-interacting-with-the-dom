579b68a5983adced4bc674231dcc78353842b3e8
Ellen Bennett <ellenbennett@Ellens-MacBook-Air.local>
2023-03-06 20:24:56 -0700

    pause/disable and resume/enable functions

--- js/challenge.js
+++ js/challenge.js
@@ -17,32 +17,56 @@ let counterDisplay = document.querySelector("h1#counter");
 
 let counter = 0;
 
-setInterval(() => {
-    counter++
-    counterDisplay.textContent = counter
-}, 1000);
+let myInterval = setInterval(countUp, 1000)
 
 // Manually inc/decrement counter w/buttons
 
 let plus = document.getElementById("plus");
 
-plus.addEventListener("click", (e) => {
+function countUp () {
     counterDisplay.textContent++;
-});
+};
+
+plus.addEventListener("click", countUp);
 
 let minus = document.getElementById("minus");
 
-minus.addEventListener("click", (e) => {
+function countDown () {
     counterDisplay.textContent--;
-});
+};
+
+minus.addEventListener("click", countDown);
 
 // Like button for numbers & log of liked numbers
 
 let liker = document.getElementById("heart");
 
-liker.addEventListener("click", (e) => {
+function liking () {
     let li = document.createElement("li")
     li.textContent = `Number ${counterDisplay.textContent} has been liked.`
     let ul = document.querySelector("ul.likes").appendChild(li)
-});
+};
 
+liker.addEventListener("click", liking);
+
+// Pause/disable & resume/enable
+
+let pause = document.getElementById("pause");
+
+function disabler () {
+    if (pause.textContent === "pause") {
+        clearInterval(myInterval)
+        plus.disabled = true
+        minus.disabled = true
+        liker.disabled = true
+        pause.textContent = "resume"
+    } else {
+        myInterval = setInterval(countUp, 1000)
+        plus.disabled = false
+        minus.disabled = false
+        liker.disabled = false
+        pause.textContent = "pause"
+    }
+};
+
+pause.addEventListener("click", disabler);
